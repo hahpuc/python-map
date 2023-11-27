60d923843bbcdef938bc5beacd779d1449b2817b
Long Nguyen <18520092@gm.uit.edu.vn>
2023-11-27 09:57:20 +0700

    Add final result

--- final-result.py
+++ final-result.py
@@ -1,26 +1,43 @@
 
 import pygmt
-
+import numpy as np
+import xarray as xr
 
 fig = pygmt.Figure()
 
-grid = pygmt.datasets.load_earth_relief(resolution="05m", region="VN")
+# Define the grid dimensions and spacing
+lon_min, lon_max, lat_min, lat_max = 102, 110, 8, 16
+lon_spacing, lat_spacing = 1, 1
+region = [lon_min, lon_max, lat_min, lat_max]
 
+# Generate synthetic data (you can replace this with your own data)
+lons = np.arange(lon_min, lon_max, lon_spacing)
+lats = np.arange(lat_min, lat_max, lat_spacing)
+lons, lats = np.meshgrid(lons, lats)
+data = np.random.randint(0, 10, size=(len(lons), len(lats)))
+
+# Create xarray.DataArray
+grid_data_array = xr.DataArray(
+    data, 
+    coords=[lats[:, 0], lons[0, :]],
+    dims=["lat", "lon"]
+)
+
+# Show the data
 fig.grdimage(
-    grid=grid,
+    grid=grid_data_array,
     cmap="haxby",
     projection="M12c",
 )
 fig.grdcontour(
     interval=250,
-    grid=grid,
+    annotation=1000,
+    grid=grid_data_array,
     limit=[-4000, -2000],
 )
 fig.coast(
-    region="VN",
+    region=region,
     projection="M12c",
-    land="lightgray",
-    water="white",
     borders="1/0.5p",
     shorelines="1/0.5p",
     frame="ag",
